f616a21fc849e5fd6ccf92c46fb2d573cd0e170f
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2023-06-15 09:24:15 +0500

    initialized code

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tether_pet_owner/src/features/authentication/signin_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: SigninScreen(),
+    );
   }
 }
